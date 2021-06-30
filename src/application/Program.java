35d64becb0a55f7c9541b5dec20e30f4997aba97
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2021-06-30 17:04:30 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,6 +13,7 @@ public class Program {
 		
 		Seller seller = new Seller(21,"Bob","bob@empresa.com",new Date(), 3000.00, obj);
 		
+		System.out.println(obj);
 		System.out.println(seller);
 		
 	}
